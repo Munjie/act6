f497a019de3ba07e288bef0816d0b29fa12ec8ea
Munjie <892790756@qq.com>
2019-04-27 19:40:05 +0800

    init

---
+++ src/main/java/com/example/act6/Act6Application.java
@@ -0,0 +1,13 @@
+package com.example.act6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Act6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Act6Application.class, args);
+    }
+
+}
